ea9c063b5f1e695c3b1479c8be201887922e5d88
Cloud Chen <pk14116@gmail.com>
2020-06-22 10:39:22 +0800

    add jwt sample code

---
+++ src/main/java/com/cmoney_training_6th/final_project_intellij/model/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.cmoney_training_6th.final_project_intellij.model;
+
+import java.io.Serializable;
+
+public class AuthenticationResponse implements Serializable {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
