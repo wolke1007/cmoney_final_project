839ea7afe24915c915444474adda757c60e08eb5
Cloud Chen <pk14116@gmail.com>
2020-06-30 13:49:46 +0800

    modify user table to fit new schema

--- src/main/java/com/cmoney_training_6th/final_project_intellij/model/User.java
+++ src/main/java/com/cmoney_training_6th/final_project_intellij/model/User.java
@@ -3,13 +3,21 @@ package com.cmoney_training_6th.final_project_intellij.model;
 import javax.persistence.*;
 
 @Entity // This tells Hibernate to make a table out of this class
-@Table(name = "user")
+@Table(name = "account")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
-    private Integer id;
+    private int account_id;
+
+    private String social_license_id;
 
     private String username;
+
+    @Column(nullable=false, length=50)
+    private String first_name;
+
+    @Column(nullable=false, length=50)
+    private String last_name;
 
     private String email;
 
@@ -17,6 +25,7 @@ public class User {
 
     private boolean active;
 
+    @Column(length=50)
     private String roles;
 
     public String getUsername() {
@@ -44,11 +53,11 @@ public class User {
     }
 
     public Integer getId() {
-        return id;
+        return account_id;
     }
 
     public void setId(Integer id) {
-        this.id = id;
+        this.account_id = id;
     }
 
     public String getName() {
