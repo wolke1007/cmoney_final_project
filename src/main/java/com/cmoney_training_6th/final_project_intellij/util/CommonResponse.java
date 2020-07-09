ed8bb27898b91f79e3aa0e96ee530c23a11ed9b0
Cloud Chen <pk14116@gmail.com>
2020-07-09 16:39:15 +0800

    improve CommonResponse make JsonArray can success output

--- src/main/java/com/cmoney_training_6th/final_project_intellij/util/CommonResponse.java
+++ src/main/java/com/cmoney_training_6th/final_project_intellij/util/CommonResponse.java
@@ -13,9 +13,17 @@ public class CommonResponse {
     public CommonResponse() {
     }
 
+    public CommonResponse(JsonArray json, int statusCode) {
+        this.statusCode = statusCode;
+        this.message = (JsonArray) this.message;
+        this.message = json;
+    }
+
     public CommonResponse(JsonObject json, int statusCode) {
+//        System.out.println("json obj construct");
         this.statusCode = statusCode;
         this.message = (JsonObject) this.message;
+        this.message = this.message.getClass();
         this.message = json;
     }
 
@@ -43,16 +51,25 @@ public class CommonResponse {
     }
 
     public JsonObject setMessage(JsonObject ret, String str) {
+//        System.out.println("string setter");
         ret.addProperty("message", str);
         return ret;
     }
 
+    public JsonObject setMessage(JsonObject ret, JsonArray json) {
+//        System.out.println("json array setter");
+        ret.add("message", json);
+        return ret;
+    }
+
     public JsonObject setMessage(JsonObject ret, JsonObject json) {
+//        System.out.println("json object setter");
         ret.add("message", json);
         return ret;
     }
 
     public JsonObject setMessage(JsonObject ret, Object message) {
+//        System.out.println("object setter");
         Gson g = new Gson();
         ret.add("message", g.toJsonTree(message).getAsJsonObject());
         return ret;
@@ -62,10 +79,23 @@ public class CommonResponse {
     public String toString() {
         JsonObject ret = new JsonObject();
         ret.addProperty("status", getStatusCode());
-        if (this.message.getClass().equals(String.class)){
-            ret = setMessage(ret, (String)this.message);
-        }else{
-            ret = setMessage(ret, this.message);
+        String cls = this.message.getClass().getSimpleName();
+        switch(cls){
+            case "JsonArray":
+                ret = setMessage(ret, (JsonArray) this.message);
+                break;
+            case "JsonObject":
+                ret = setMessage(ret, (JsonObject) this.message);
+                break;
+            case "String":
+                ret = setMessage(ret, (String) this.message);
+                break;
+            case "Object":
+                ret = setMessage(ret, this.message);
+                break;
+            default:
+                ret = setMessage(ret, "not supported data type: " + cls);
+                break;
         }
         return ret.toString();
     }
