c902851d4768af3053ac314dbe9d7cb18cd4c5e3
Cloud Chen <pk14116@gmail.com>
2020-07-19 18:15:01 +0800

    modify comment unit test module import which cause build fail

--- src/main/java/com/cmoney_training_6th/final_project_intellij/FinalProjectIntellijApplication.java
+++ src/main/java/com/cmoney_training_6th/final_project_intellij/FinalProjectIntellijApplication.java
@@ -1,23 +1,13 @@
 package com.cmoney_training_6th.final_project_intellij;
 
-import com.cmoney_training_6th.final_project_intellij.model.Schedule;
-import com.cmoney_training_6th.final_project_intellij.repos.DoctorRepository;
-import com.cmoney_training_6th.final_project_intellij.repos.ScheduleRepository;
 import com.cmoney_training_6th.final_project_intellij.repos.UserRepository;
 import com.cmoney_training_6th.final_project_intellij.services.FilesStorageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import javax.annotation.Resource;
-import java.io.File;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 
 
 @SpringBootApplication
