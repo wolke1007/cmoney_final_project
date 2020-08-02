8db7a811d1320e6dfbf098e401ae08072ebaea17
Unknown <pk14116@gmail.com>
2020-08-02 17:19:13 +0800

    move MyUserDetailService to service dir

---
+++ src/main/java/com/cmoney_training_6th/final_project_intellij/implenment/MyUserDetailsServiceImpl.java
@@ -0,0 +1,83 @@
+package com.cmoney_training_6th.final_project_intellij.implenment;
+
+import com.cmoney_training_6th.final_project_intellij.model.User;
+import com.cmoney_training_6th.final_project_intellij.dao.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class MyUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + username));
+        return user.map(MyUserDetails::new).get();
+    }
+
+    public static class MyUserDetails implements UserDetails {
+
+        private String username;
+        private String password;
+        private boolean active;
+        private List<GrantedAuthority> authorities;
+
+        public MyUserDetails(User user){
+            this.username = user.getUsername();
+            this.password = user.getPassword();
+            this.active = user.isActive();
+            this.authorities = Arrays.stream(user.getRole().split(","))
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return authorities;
+        }
+
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        @Override
+        public String getUsername() {
+            return username;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return active;
+        }
+    }
+}
